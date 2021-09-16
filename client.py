af52c98f7ece04b05314774b99842bb779247a72
Andrea Elias <42878138+andreaeliasc@users.noreply.github.com>
2021-09-15 21:45:02 -0600

    updating

--- client.py
+++ client.py
@@ -7,6 +7,7 @@
 
 import socket
 import threading
+import pickle
 
 inicioPartida = False
 
@@ -23,14 +24,41 @@ CARDBOMB = 8
 
 #!/usr/bin/env python3
 
-import socket
+# import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+# HOST = '127.0.0.1'  # The server's hostname or IP address
+# PORT = 65432        # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.connect((HOST, PORT))
+#     s.sendall(b'Hello, world')
+#     data = s.recv(1024)
 
-print('Received', repr(data))
+# print('Received', repr(data))
+
+def send():
+    global inicioPartida
+    while True:
+        print(
+            '''
+            \nTienes las siguientes opciones
+            \n1. Iniciar la partida
+            \n2. Enviar Mensaje
+            \n3. Realizar Jugada
+            \n4. Hacer un Favor
+            \n5. Ver estado del juego
+            \n6. Ver cartas que tengo
+            \n7. Ver lista de jugadores conectados
+            \n8. Instrucciones
+            '''
+        )
+        opcion = int(input('\nIngresa el numero de opcion > '))
+        if opcion == 1:
+            if inicioPartida == False:
+                objeto = {
+                    'opcion': opcion
+                }
+                data_string = pickle.dumps(objeto)
+                cli_sock.send(data_string) 
+            else:
+                print('La partida ya fue iniciada.')
---
+++ utils.py
@@ -0,0 +1,36 @@
+'''
+    File name: client.py
+    Author: Andrea Elías, Diego Estrada & Luis Urbina
+    Date created: 10/09/2021
+    Python Version: 3.7
+'''
+
+from random import sample
+
+Mazo=[]
+MP1=[]
+MP2=[]
+MP3=[]
+MP4=[]
+MP5=[]
+Carta1="Va a ser que no"
+Carta2="Ataque"
+Carta3="Pasar"
+Carta4="Favor"
+Carta5="Barajar"
+Carta6="Ver el futuro"
+Carta7="Gato1"
+Carta8="Gato2"
+Carta9="Gato3"
+Carta10="Gato4"
+Carta11="Salvacion"
+Carta12="Bomba"
+
+
+
+
+
+
+
+
+
